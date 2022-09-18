0cc45d961ef301fede140aa9d87392353c1110ec
P@ssW0rdgithub <mohamedelkordy020@gmail.com>
2022-09-18 14:17:32 +0200

    ToggleCartBag

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,21 @@
+import { useContext } from 'react';
 
+import ProductCard from '../../components/product-card/product-card.component';
 
+import { ProductsContext } from '../../contexts/products.context';
 
-const Shop=()=>{
+import './shop.styles.scss';
 
+const Shop = () => {
+  const { products } = useContext(ProductsContext);
 
-    return (<div>
+  return (
+    <div className='products-container'>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
 
-        <h1>I'm Shop</h1>
-    </div>)
-}
-
-
-
-
-
-export default Shop
+export default Shop;
